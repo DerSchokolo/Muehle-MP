5f381cd614edf9518eaaf088cb821e8526e352d7
DerSchokolo <frfranzen@gmail.com>
2023-02-22 16:32:41 +0100

    detectmuehle horizontal finished

--- src/web/resources/static/functions.js
+++ src/web/resources/static/functions.js
@@ -22,7 +22,36 @@ function board(field, color) {
 };
 
 // creates var vor every fieled on the board
-boardarray = [];
+let boardarray = [];
 for (i=0; i<24; i++) {
     boardarray[i] = new board(i, 'empty');
+};
+
+// checks if muehle was detectet
+function detectmuehle(boardarray) {
+
+    // horizontal muehle
+    let color = "white";
+    for (i=0; i<2; i++) {
+        let counter = 0;
+        for (j=0; j<8; j++) {
+            if (boardarray[counter].color == color && boardarray[counter+1].color == color && boardarray[counter+2].color == color) {
+                console.log('mühle detectet');
+                return;
+            };
+            counter = counter + 3;
+        };
+        color = "black";
+    }; 
+
+    // vertical muehle
+    color = "white";
+    for (i=0; i<2; i++) {
+        if (//TODO Statements) {
+            console.log('mühle detectet');
+            return;
+        };
+        color = "black";
+    };
+    console.log('no mühle detectet'); 
 };
