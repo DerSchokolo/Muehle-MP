07fd7c81e27d0e77f583b4203d41c7f5aa63452d
DerSchokolo <76744587+DerSchokolo@users.noreply.github.com>
2023-02-22 18:33:22 +0100

    finished detection of muehle

--- src/web/resources/static/functions.js
+++ src/web/resources/static/functions.js
@@ -1,3 +1,4 @@
+// print input from grid in console
 function detectinput() {
     console.log('input detectet field: ' + this.id);
 };
@@ -28,30 +29,132 @@ for (i=0; i<24; i++) {
 };
 
 // checks if muehle was detectet
+// returns  'white' for white muehle 
+//          'black' for black muehle
+//          'nomuehle' for no detectet muehle
 function detectmuehle(boardarray) {
 
-    // horizontal muehle
+    // vertical muehle
     let color = "white";
     for (i=0; i<2; i++) {
         let counter = 0;
         for (j=0; j<8; j++) {
             if (boardarray[counter].color == color && boardarray[counter+1].color == color && boardarray[counter+2].color == color) {
-                console.log('mühle detectet');
-                return;
+                console.log('mühle detectet')
+                if (color == 'white') {
+                    return 'white';
+                };
+    
+                if (color == 'black') {
+                    return 'black';
+                };
             };
             counter = counter + 3;
         };
         color = "black";
     }; 
 
-    // vertical muehle
+    // horizontal muehle
     color = "white";
     for (i=0; i<2; i++) {
-        if (//TODO Statements) {
+        // vertical 0-9-21
+        if (boardarray[0].color == color && boardarray[9].color == color && boardarray[21].color == color) {
             console.log('mühle detectet');
-            return;
+            if (color == 'white') {
+                return 'white';
+            };
+
+            if (color == 'black') {
+                return 'black';
+            };
+        };
+
+        // horizontal 3-10-18
+        if (boardarray[3].color == color && boardarray[10].color == color && boardarray[18].color == color) {
+            console.log('mühle detectet');
+            if (color == 'white') {
+                return 'white';
+            };
+
+            if (color == 'black') {
+                return 'black';
+            };
+        };
+
+        // horizontal 6-11-15
+        if (boardarray[6].color == color && boardarray[11].color == color && boardarray[15].color == color) {
+            console.log('mühle detectet');
+            if (color == 'white') {
+                return 'white';
+            };
+
+            if (color == 'black') {
+                return 'black';
+            };
+        };
+
+        // horizontal 1-4-7
+        if (boardarray[1].color == color && boardarray[4].color == color && boardarray[7].color == color) {
+            console.log('mühle detectet');
+            if (color == 'white') {
+                return 'white';
+            };
+
+            if (color == 'black') {
+                return 'black';
+            };
+        };
+
+        // horizontal 16-19-22
+        if (boardarray[16].color == color && boardarray[19].color == color && boardarray[22].color == color) {
+            console.log('mühle detectet');
+            if (color == 'white') {
+                return 'white';
+            };
+
+            if (color == 'black') {
+                return 'black';
+            };
+        };
+
+        // horizontal 8-12-17
+        if (boardarray[8].color == color && boardarray[12].color == color && boardarray[17].color == color) {
+            console.log('mühle detectet');
+            if (color == 'white') {
+                return 'white';
+            };
+
+            if (color == 'black') {
+                return 'black';
+            };
+        };
+
+        // horizontal 5-13-20
+        if (boardarray[5].color == color && boardarray[13].color == color && boardarray[20].color == color) {
+            console.log('mühle detectet');
+            if (color == 'white') {
+                return 'white';
+            };
+
+            if (color == 'black') {
+                return 'black';
+            };
+        };
+
+        // horizontal 2-14-23
+        if (boardarray[2].color == color && boardarray[13].color == color && boardarray[23].color == color) {
+            console.log('mühle detectet');
+            if (color == 'white') {
+                return 'white';
+            };
+
+            if (color == 'black') {
+                return 'black';
+            };
         };
         color = "black";
     };
-    console.log('no mühle detectet'); 
+    console.log('keine mühle detectet')
+    return 'nomuehle';
 };
+
