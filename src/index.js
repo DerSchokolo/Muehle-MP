4c40f2c693e8d5d27c473f217dcca8e7930837b9
DerSchokolo <frfranzen@gmail.com>
2023-03-06 18:45:32 +0100

    added admin panel

--- src/index.js
+++ src/index.js
@@ -3,7 +3,15 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
+const { instrument } = require('@socket.io/admin-ui')
+const io = new Server(server, {
+
+    // admin panel
+    cors: {
+      origin: ["https://admin.socket.io"],
+      credentials: true
+    }
+});
 
 // serves index html
 app.get('/', (req, res) => {
@@ -14,6 +22,15 @@ app.get('/', (req, res) => {
 app.get('/game', (req, res) => {
     res.sendFile(__dirname + '/game.html');
 });
+
+io.on("connection", (socket) => {
+    socket.on("roomnumber", (arg) => {
+        socket.join(arg);
+        console.log(arg); // world
+        console.log(socket);
+    });
+  });
+
 
 // serves lobby html with id of the room
 app.get('/lobby:id', (req, res) => {
@@ -27,6 +44,12 @@ app.use(express.static('public'));
 // logs connection
 io.on('connection', (socket) => {
     console.log('a user connected');
+});
+
+// admin panel
+instrument(io, {
+    auth: false,
+    mode: "development",
 });
 
 // starts server on port 3000
